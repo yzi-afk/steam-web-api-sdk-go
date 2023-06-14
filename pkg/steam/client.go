1a03c699c0d9d09b33db346536617d654e13c457
Rapipat <rapipat.udo@gmail.com>
2023-06-14 19:17:38 +0700

    Add client methods

--- pkg/steam/client.go
+++ pkg/steam/client.go
@@ -1,13 +1,23 @@
 package steam
 
 import (
+	"context"
 	"errors"
+	"io"
 	"net/http"
+	"net/url"
+)
+
+const (
+	SteamAPIBaseURL = "https://api.steampowered.com/"
+
+	APIEndpointGetAppList = "ISteamApps/GetAppList/v2/"
 )
 
 type Client struct {
-	apiKey     string
-	httpClient *http.Client
+	apiKey          string
+	httpClient      *http.Client
+	endpointBaseURL *url.URL
 }
 
 func New(apiKey string) (*Client, error) {
@@ -19,4 +29,66 @@ func New(apiKey string) (*Client, error) {
 		apiKey:     apiKey,
 		httpClient: http.DefaultClient,
 	}, nil
+}
+
+func (c *Client) endpointURL(endpoint string) (*url.URL, error) {
+	u, err := url.Parse(SteamAPIBaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	u.Path += endpoint
+}
+
+func (c *Client) do(ctx context.Context, req *http.Request) (*http.Response, error) {
+	if ctx != nil {
+		req = req.WithContext(ctx)
+	}
+	return c.httpClient.Do(req)
+}
+
+func (c *Client) get(ctx context.Context, endpoint string, query url.Values) (*http.Response, error) {
+	u, err := c.endpointURL(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	if query != nil {
+		u.RawQuery = query.Encode()
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.do(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return resp, nil
+
+}
+
+func (c *Client) post(ctx context.Context, endpoint string, body io.Reader) (*http.Response, error) {
+	u, err := c.endpointURL(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, u.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.do(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return resp, nil
+
 }
